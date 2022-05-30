1137827931e410445f1442aca155bd8d24ceaa6a
Sebastian Törsleff <sebastian.toersleff@hsu-hh.de>
2022-05-30 19:27:32 +0200

    Added second validation example project for OptiFlex

---
+++ eclipseProjects/de.enflexit.awb.sa/examples/net.optiFlexIteration2.validationExample/src/optiFlexIteration2/OptimizationAgent.java
@@ -0,0 +1,63 @@
+package optiFlexIteration2;
+
+import java.io.File;
+import java.util.Set;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.reasoner.ValidityReport;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+
+import agentgui.core.application.Application;
+import de.enflexit.awb.sa.core.KnowledgeBase;
+import de.enflexit.awb.sa.core.OwlMessageReceiveBehaviour;
+import de.enflexit.awb.sa.core.SendInformBehaviour;
+import de.enflexit.awb.sa.core.SendQueryBehaviour;
+import de.enflexit.awb.sa.core.UtilityMethods;
+import jade.core.AID;
+import jade.core.Agent;
+
+
+/**
+ * @author Sebastian Törsleff, Helmut Schmidt University
+ */
+public class OptimizationAgent extends Agent {
+
+
+	// --- static variables -----------------------------
+	private static final long serialVersionUID = 3164333512327730902L;
+	private static Logger rootLogger = Logger.getRootLogger();
+	
+	
+	// --- conversation ID variables
+	private String cidBase;
+	protected static int cidCnt = 0;
+	
+	@Override
+	protected void setup() {
+		
+		// --- Logger configuration --------------------------------------
+		rootLogger.removeAllAppenders(); //unschöner workaround. sollte besser an zentraler Stelle erfolgen und nicht bei jedem Agenten individuell
+		SimpleLayout layout = new SimpleLayout();
+		ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+		rootLogger.addAppender(consoleAppender);
+		rootLogger.setLevel(Level.DEBUG);			
+		
+		// Timeblocker 2s for setting up JADE sniffer
+		try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+		
+	
+	}
+
+}
