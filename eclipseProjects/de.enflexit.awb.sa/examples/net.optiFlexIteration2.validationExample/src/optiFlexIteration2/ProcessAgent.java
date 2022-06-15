465a895994bad6f4e742f1b27f0386497458a09b
Sebastian Törsleff <sebastian.toersleff@hsu-hh.de>
2022-06-15 18:57:44 +0200

    New classes for OptiFlex Validation 2

--- eclipseProjects/de.enflexit.awb.sa/examples/net.optiFlexIteration2.validationExample/src/optiFlexIteration2/ProcessAgent.java
+++ eclipseProjects/de.enflexit.awb.sa/examples/net.optiFlexIteration2.validationExample/src/optiFlexIteration2/ProcessAgent.java
@@ -27,26 +27,144 @@ public class ProcessAgent extends Agent {
 	// --- static variables -----------------------------
 	private static final long serialVersionUID = 3164333512327730902L;
 	private static Logger rootLogger = Logger.getRootLogger();
-	
-	
-	// --- conversation ID variables 
+
+	// --- obligatory variables for a semantic agent
+	private KnowledgeBase knowledgeBase;
+	private OwlMessageReceiveBehaviour owlMsgReceiveBehaviour;
+	private String ontologyName; 
+
+	// --- variables required for evaluation
+	private AID communicationPartner;
+
+	// --- conversation ID variables
 	private String cidBase;
 	protected static int cidCnt = 0;
-	
+
+
 	@Override
 	protected void setup() {
-		
 
-		
+		// -------------------------------------------------------------------
+		// --- obligatory setup for a semantic agent -------------------------
+		// -------------------------------------------------------------------
+
+		// --- for now only one ontology is supported; this variable is used ------------
+		// --- for the ontology field of ACL message objects and message filtering; -----
+		this.ontologyName = "OptiFlex_20220318";
+
+		// --- specify ontology folder path and file name --------------------------
+		String ontologyDirectory = Application.getProjectFocused().getProjectFolderFullPath()
+				+ "knowledgeBases" + File.separator 
+				+ Application.getProjectFocused().getSimulationSetupCurrent() + File.separator 
+				+ this.getAID().getLocalName();
+		String ontologyFileName = "OptiFlex_20220318.owl";
+
+		// --- specify Base URI ------------------------
+		String baseUri = "http://www.hsu-ifa.de/ontologies/OptiFlex#"; 
+
+		// --- instantiate knowledge base with previously defined parameters -----------------
+		this.knowledgeBase = new KnowledgeBase(this, ontologyDirectory, ontologyFileName, baseUri);
+
+		// --- add individual namespaces --------------
+		knowledgeBase.getNamespaceList().addNameSpace("", baseUri, false);
+
+		// --- Determine communication partner ----------------
+		this.communicationPartner = new AID("OptimizationAgent", AID.ISLOCALNAME);
+
+
+		// --- Generate set of trusted agents used by OMRB for handling of incoming messages
+		Set<AID> trustedAgents = Set.of(this.communicationPartner); 
+
+
+		// --- add OWL message receive behavior ----------------------
+		this.owlMsgReceiveBehaviour = new OwlMessageReceiveBehaviour(this.ontologyName, this, this.knowledgeBase, trustedAgents);
+		this.addBehaviour(this.owlMsgReceiveBehaviour);
+
 		// --- Logger configuration --------------------------------------
-		rootLogger.removeAllAppenders(); //unschöner workaround. sollte besser an zentraler Stelle erfolgen und nicht bei jedem Agenten individuell
+		rootLogger.removeAllAppenders(); 
 		SimpleLayout layout = new SimpleLayout();
 		ConsoleAppender consoleAppender = new ConsoleAppender(layout);
 		rootLogger.addAppender(consoleAppender);
 		rootLogger.setLevel(Level.DEBUG);	
-		
+
 		// Timeblocker 2s for setting up JADE sniffer
 		try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
-		
+
+		testSendInformBehaviour();
 	}
+
+	@Override
+	protected void takeDown() {
+
+		// --- Save the knowledge base model into a file, as the current implementation ---
+		// --- does not use a persistent storage ------------------------------------------
+		this.knowledgeBase.saveModel();
+
+		// --- Close the knowledge base model ---------------------------------------------
+		this.knowledgeBase.closeModel();
+
+
+		rootLogger.removeAllAppenders(); 		
+
+		super.takeDown();
+	}
+
+	/**
+	 * Method to generate a unique conversation ID for ACL messages
+	 * @return String with the conversation ID
+	 */
+	public String generateArbitraryConversationId() {
+		if (cidBase==null) {
+			cidBase = this.getLocalName() + hashCode() +System.currentTimeMillis()%10000 + "_";		
+		}
+		return cidBase + (cidCnt++);
+	}
+
+	private void testSendInformBehaviour() {
+		
+		String processName; 
+		if(this.getLocalName().equals("ProcessAgent")) {
+			processName = "process1";
+		}
+		else {
+			processName = "process2";
+		}
+
+		String queryResults = null;
+
+		String nodeStateQuery = "CONSTRUCT {?s ?p ?o}\n"
+				+ "WHERE {\n"
+				+ "    ?s ?p ?o {\n"
+				+ "        SELECT ?s ?o\n"
+				+ "        WHERE {\n"
+				+ "            {\n"
+				+ "                ?process :hasFlexibleLoad ?s.\n"
+				+ "                FILTER (?process = :"+ processName +" )\n"
+				+ "            }\n"
+				+ "            UNION\n"
+				+ "            {\n"
+				+ "                ?process :hasFlexibleLoad ?o.\n"
+				+ "                FILTER (?process = :"+ processName +")\n"
+				+ "            }\n"
+				+ "            UNION \n"
+				+ "            {\n"
+				+ "                ?s :hasTriggerFlexibleLoad ?fl.\n"
+				+ "                ?process :hasFlexibleLoad ?fl\n"
+				+ "                FILTER (?process = :"+ processName +")\n"
+				+ "            }\n"
+				+ "        }        \n"
+				+ "    }\n"
+				+ "}";
+
+
+		String nodeStateQueryPSS = UtilityMethods.addPrefixesToSparqlQuery(nodeStateQuery, knowledgeBase);
+		queryResults = UtilityMethods.executeConstructQuery(nodeStateQueryPSS, this.knowledgeBase.getModel());
+
+		rootLogger.debug(queryResults);
+
+		SendInformBehaviour sendInformBehaviour = new SendInformBehaviour(this.communicationPartner, queryResults, this.generateArbitraryConversationId(), ontologyName);
+		this.addBehaviour(sendInformBehaviour);
+
+	}
+
 }
